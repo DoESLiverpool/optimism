3bed71f94393368ec55584cd0873c3c435a62327
Adrian McEwen <adrianm@mcqn.com>
2020-03-03 21:04:32 +0000

    Set up basic link to sqlite3 database for development.  Part of #3

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const Sequelize = require('sequelize')
 const app = express()
 const port = 3000
+
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: 'db/development.sqlite'
+})
 
 app.get('/', (req, res) => res.send('Hello Optimism!'))
 
