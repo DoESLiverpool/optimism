5851eaa64f73374d30d732fd83dfd946e74e5ef7
Christopher Thompson <chris.thompson@helicalbytes.org>
2020-08-20 18:24:49 +0100

    Hard-coded response for calendar endpoint

--- api/routes/calendar.js
+++ api/routes/calendar.js
@@ -3,9 +3,28 @@ const express = require('express');
 const router = express.Router()
 module.exports = router;
 
-router.get('/', function(req, res) {
-    res.json({
-        source: req.baseUrl,
-        method: req.method
-    });
+example_calendar = {
+    date: '2020-08-17',
+    resources: [
+        {
+            id: 3,
+            name: 'Large laser cutter (Gerald)',
+            slots: [
+                { starts: '09:00', duration: '4:30', repeats: 1 },
+                { starts: '13:30', duration: '3:30', repeats: 1 }
+            ]
+        },
+        {
+            id: 4,
+            name: 'Events room',
+            slots: [
+                { starts: '09:00', duration: '1:00', repeats: 12 }
+            ]
+        }
+    ]
+}
+
+router.get('/', function (req, res) {
+    res.json(example_calendar);
 });
+
