8bbc711456ef74ab56c95b2cb38f74ca998a7921
Christopher Thompson <chris.thompson@helicalbytes.org>
2020-08-30 20:03:32 +0100

    Minor change for sqlite knex config

--- api/db.js
+++ api/db.js
@@ -2,7 +2,8 @@ var knex = require('knex')({
     client: 'sqlite3',
     connection: {
         filename: "./db/optimism.sqlite3"
-    }
+    },
+    useNullAsDefault: true
 });
 
 module.exports = knex;
