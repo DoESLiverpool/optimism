ceb11c49ba961858b834e738924c3046ae5e3602
Christopher Thompson <chris.thompson@helicalbytes.org>
2020-08-30 20:02:48 +0100

    Separate file for select-a-time route

---
+++ website/routes/select-a-time.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const axios = require('axios');
+const utilities = require('../utilities');
+const settings = require('../settings');
+
+const apiUrl = settings.apiUrl;
+const router = express.Router()
+module.exports = router;
+
+router.get('/select-a-time', function (req, res) {
+
+    const resourceId = req.query.resource;
+
+    if (!resourceId) {
+        console.log('No resource id');
+    }
+
+    const resourceUrl = `${apiUrl}/resources/${resourceId}`;
+    const calendarUrl = `${apiUrl}/calendar`;
+    const templateVariables = {};
+
+    axios.get(resourceUrl)
+        .then(function (response) {
+            templateVariables.resource = response.data;
+            return axios.get(calendarUrl);
+        })
+        .then(function(response) {
+            templateVariables.calendar = response.data;
+            res.render('select-a-time.html', templateVariables);
+        })
+        .catch(function (error) {
+            console.log(error);
+            res.render('error.html', {
+                safeErrorMessage: utilities.safeErrorMessage(error.message)
+            });
+        });
+
+});
