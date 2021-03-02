3185f2011c8757cdbac8f8db4e0ef94c782496b9
jackie1050 <jackiepease@hotmail.com>
2021-03-02 20:58:13 +0000

    Added test folder

---
+++ test/helpers/checkDay.test.js
@@ -0,0 +1,20 @@
+// const {chai} = require('chai')
+// const expect = require('chai').expect
+// const {moment} = require('moment')
+
+// // Example of how you could unit test a function, that has been used in calendar.js
+
+// // pull it out into a separate file, check that it returns what you expect. 
+
+// const {checkDay} = require('/home/jake/Desktop/optimism/api/helpers/checkDay.js')
+
+// describe('checkDay helper function test suite', () => {
+//   it('checks that checkDay returns true', () => {
+//     let date = 0;
+//     let slot = 1
+
+//     let response = checkDay(date, slot)
+
+//     expect(response).to.be(true)
+//   })
+// })
