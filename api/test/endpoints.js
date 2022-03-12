cdfa7d30d40bb32572445b71651af055e4f2f7a4
helicalbytes <c.thompson@helicalbytes.org>
2022-03-12 15:29:02 +0000

    Formatting changes for endpoint tests

--- api/test/endpoints.js
+++ api/test/endpoints.js
@@ -4,28 +4,26 @@ const request = require("supertest");
 
 const app = require("../app");
 
-describe('app.js integration test', () => {
+describe('Optimism api endpoint tests', () => {
+
   it('Returns a 404 status on invalid /resource endpoint', async () => {
-    const response = await request(app)
-      .get('/api/resources/nothing')
-    expect(response.status).to.equal(404)
-    expect(response.error.text).to.equal("No such resource")
+    const response = await request(app).get('/api/resources/nothing');
+    expect(response.status).to.equal(404);
+    expect(response.error.text).to.equal("No such resource");
   });
 
   it('Returns a 200 status on valid /resource endpoint', async () => {
-    const response = await request(app)
-      .get('/api/resources/')
-    expect(response.status).to.equal(200)
+    const response = await request(app).get('/api/resources/');
+    expect(response.status).to.equal(200);
   });
+
   it('Returns a 404 status on invalid /calendar endpoint', async () => {
-    const response = await request(app)
-      .get('/api/calendar/nothing')
-    expect(response.status).to.equal(404)
+    const response = await request(app).get('/api/calendar/nothing');
+    expect(response.status).to.equal(404);
   });
 
   it('Returns a 200 status on valid /calendar endpoint', async () => {
-    const response = await request(app)
-      .get('/api/calendar/2021-04-06/2021-04-14/1')
-    expect(response.status).to.equal(200)
+    const response = await request(app).get('/api/calendar/2021-04-06/2021-04-14/1');
+    expect(response.status).to.equal(200);
   });
 });
