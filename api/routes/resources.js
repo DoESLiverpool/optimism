c3c2d38fc189ca1bb1a4ace8cb0448b4ee502d6d
helicalbytes <c.thompson@helicalbytes.org>
2022-03-16 22:33:00 +0000

    Simplified resource route get handler

--- api/routes/resources.js
+++ api/routes/resources.js
@@ -3,25 +3,21 @@ const mainModel = require('../model');
 const router = express.Router()
 module.exports = router;
 
-router.get('/:resourceId?', function (req, res) {
+router.get('/:resourceId?', async function (req, res) {
 
     const queryId = req.params.resourceId;
 
     if (queryId) {
         let id = Number(queryId);
         if (!Number.isInteger(id) || id < 0) {
-            res.status(400).send(`Resource id (${queryId}) is not valid.`);
+            res.status(400).send(`Resource id is not valid.`);
             return;
         }
 
-        (async function () {
-            const resource = await mainModel.resources.get(id);
-            resource == null ? res.status(404).send('No such resource') : res.json(resource);
-        }());
+        const resource = await mainModel.resources.get(id);
+        resource == null ? res.status(404).send('No such resource') : res.json(resource);
     } else {
-        (async function () {
-            const resources = await mainModel.resources.getAll();
-            res.json(resources);
-        }());
+        const resources = await mainModel.resources.getAll();
+        res.json(resources);
     }
 });
