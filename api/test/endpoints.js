19366f165f35a23d555b3863de6abd0931bdfca7
helicalbytes <c.thompson@helicalbytes.org>
2022-03-17 02:38:40 +0000

    Refactored tests

--- api/test/endpoints.js
+++ api/test/endpoints.js
@@ -4,26 +4,26 @@ const request = require("supertest");
 
 const app = require("../app");
 
-describe('Optimism api endpoint tests', () => {
-
-  it('Returns a 404 status on invalid /resource endpoint', async () => {
-    const response = await request(app).get('/api/resources/nothing');
-    expect(response.status).to.equal(404);
-    expect(response.error.text).to.equal("No such resource");
+describe('API', function () {
+  describe('/api/resources', () => {
+    it('returns an HTTP 400 status if an invalid resource id is provided', async () => {
+      const response = await request(app).get('/api/resources/not-an-integer');
+      expect(response.status).to.equal(400);
+      expect(response.error.text).to.equal("Resource id is not valid.");
+    });
+    it('returns an HTTP 200 status if no resource id is provided', async () => {
+      const response = await request(app).get('/api/resources/');
+      expect(response.status).to.equal(200);
+    });
   });
-
-  it('Returns a 200 status on valid /resource endpoint', async () => {
-    const response = await request(app).get('/api/resources/');
-    expect(response.status).to.equal(200);
+  describe('/api/calendar', () => {
+    it('Returns a 404 status on invalid /calendar endpoint', async () => {
+      const response = await request(app).get('/api/calendar/nothing');
+      expect(response.status).to.equal(404);
+    });
+    it('Returns a 200 status on valid /calendar endpoint', async () => {
+      const response = await request(app).get('/api/calendar/2021-04-06/2021-04-14/1');
+      expect(response.status).to.equal(200);
+    });
   });
-
-  it('Returns a 404 status on invalid /calendar endpoint', async () => {
-    const response = await request(app).get('/api/calendar/nothing');
-    expect(response.status).to.equal(404);
-  });
-
-  it('Returns a 200 status on valid /calendar endpoint', async () => {
-    const response = await request(app).get('/api/calendar/2021-04-06/2021-04-14/1');
-    expect(response.status).to.equal(200);
-  });
-});
+})
