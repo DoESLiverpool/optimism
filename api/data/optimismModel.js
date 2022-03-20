e2b41b90ff28383f9a02d0cd051743679574fb29
helicalbytes <c.thompson@helicalbytes.org>
2022-03-20 21:50:26 +0000

    Added ResourceTypeItems and added it to model

--- api/data/optimismModel.js
+++ api/data/optimismModel.js
@@ -1,14 +1,14 @@
 const ResourceItems = require('./resourceItems');
+const ResourceTypeItems = require('./resourceTypeItems');
 const SlotItems = require('./slotItems');
-const CalendarItems = require('./calendarItems');
 const BookingItems = require('./bookingItems');
 
 class OptimismModel {
   constructor (knex) {
     this.knex = knex;
     this.resources = new ResourceItems(this);
+    this.resourceTypes = new ResourceTypeItems(this);
     this.slots = new SlotItems(this);
-    this.calendar = new CalendarItems(this);
     this.bookings = new BookingItems(this);
   }
 }
