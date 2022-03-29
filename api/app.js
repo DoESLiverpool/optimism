13706164c7ad848d414aa8bb0bcd2b276b80625f
helicalbytes <c.thompson@helicalbytes.org>
2022-03-29 20:22:41 +0100

    Add routes for resource types

--- api/app.js
+++ api/app.js
@@ -3,9 +3,10 @@ const cors = require('cors');
 const app = express();
 
 require('dotenv').config();
-const port = process.env.OPTIMISM_API_PORT || 3001
+const port = process.env.OPTIMISM_API_PORT || 3001;
 
 const resourceRoutes = require('./routes/resources');
+const resourceTypeRoutes = require('./routes/resourceTypes');
 const bookingRoutes = require('./routes/bookings');
 const calendarRoutes = require('./routes/calendar');
 const slotRoutes = require('./routes/slots');
@@ -13,10 +14,11 @@ const slotRoutes = require('./routes/slots');
 app.use(cors());
 app.use(express.json());
 app.use('/api/resources', resourceRoutes);
+app.use('/api/resource-types', resourceTypeRoutes);
 app.use('/api/bookings', bookingRoutes);
 app.use('/api/calendar', calendarRoutes);
 app.use('/api/slots', slotRoutes);
 
-app.listen(port, () => console.log(`Optimism api listening on port ${port}.`))
+app.listen(port, () => console.log(`Optimism api listening on port ${port}.`));
 
-module.exports = app
+module.exports = app;
