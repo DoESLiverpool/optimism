d83c475ec8824e85e6820716c113e81ef813e5e5
Adrian McEwen <adrianm@mcqn.com>
2026-02-03 20:18:20 +0000

    Allow email config to be provided in .env as well as in `/run/...` secret files.  Part of #74

--- api/app.js
+++ api/app.js
@@ -7,17 +7,17 @@ require('dotenv').config();
 const port = process.env.OPTIMISM_API_PORT || 3001;
 
 // Read in the email config from the secrets
-var email_user = "NEED TO SET EMAIL USER";
-var email_pass = "NEED TO SET EMAIL PASS";
+process.env.OPTIMISM_EMAIL_USER || (process.env.OPTIMISM_EMAIL_USER = "NEED TO SET EMAIL USER");
+process.env.OPTIMISM_EMAIL_PASS || (process.env.OPTIMISM_EMAIL_PASS = "NEED TO SET EMAIL PASS");
 const email_user_path = "/run/secrets/email_user";
 const email_pass_path = "/run/secrets/email_pass";
 if (fs.existsSync(email_user_path))
 {
-    email_user = fs.readFileSync(email_user_path, { encoding: 'utf8' }).trim();
+    process.env.OPTIMISM_EMAIL_USER = fs.readFileSync(email_user_path, { encoding: 'utf8' }).trim();
 }
 if (fs.existsSync(email_pass_path))
 {
-    email_pass = fs.readFileSync(email_pass_path, { encoding: 'utf8' }).trim();
+    process.env.OPTIMISM_EMAIL_PASS = fs.readFileSync(email_pass_path, { encoding: 'utf8' }).trim();
 }
 
 const resourceRoutes = require('./routes/resources');
