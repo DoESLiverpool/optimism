8be26cb607b92d803bceb302508ef225ccfd573f
Adrian McEwen <adrianm@mcqn.com>
2026-08-04 20:53:26 +0100

    Add admin user/password to the deployed secrets.  Part of #74

--- website/settings.js
+++ website/settings.js
@@ -1,5 +1,17 @@
+const fs = require('fs');
+
 require('dotenv').config();
 
+const admin_user_path = "/run/secrets/admin_user";
+const admin_pass_path = "/run/secrets/admin_pass";
+if (fs.existsSync(admin_user_path))
+{
+  process.env.OPTIMISM_ADMIN_USERNAME = fs.readFileSync(admin_user_path, { encoding: 'utf8' }).trim();
+}
+if (fs.existsSync(admin_pass_path))
+{
+  process.env.OPTIMISM_ADMIN_PASSWORD = fs.readFileSync(admin_pass_path, { encoding: 'utf8' }).trim();
+}
 const settings = {
   port: process.env.OPTIMISM_WEBSITE_PORT || 3000,
   apiUrl: process.env.OPTIMISM_API_URL || 'http://optimism_api:3001/api',
